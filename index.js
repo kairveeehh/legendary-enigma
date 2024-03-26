d854c12b2f28029e7246f10e4d9f7cb8fe5ad0b4
kairveeehh <jampu741@gmail.com>
2024-03-26 08:29:46 +0530

    bug fixing

--- index.js
+++ index.js
@@ -2,44 +2,46 @@ import http from 'http'
 import path from 'path'
 import { spawn } from 'child_process'
 import express from 'express'
-import {Server as SocketIO } from 'socket.io'
+import { Server as SocketIO } from 'socket.io'
 
 const app = express();
 const server = http.createServer(app);
 const io = new SocketIO(server)
+
 const options = [
-  '-i',
-  '-',
-  '-c:v', 'libx264',
-  '-preset', 'ultrafast',
-  '-tune', 'zerolatency',
-  '-r', `${25}`,
-  '-g', `${25 * 2}`,
-  '-keyint_min', 25,
-  '-crf', '25',
-  '-pix_fmt', 'yuv420p',
-  '-sc_threshold', '0',
-  '-profile:v', 'main',
-  '-level', '3.1',
-  '-c:a', 'aac',
-  '-b:a', '128k',
-  '-ar', 128000 / 4,
-  '-f', 'flv',
-  `rtmp://a.rtmp.youtube.com/live2/`,
+    '-i',
+    '-',
+    '-c:v', 'libx264',
+    '-preset', 'ultrafast',
+    '-tune', 'zerolatency',
+    '-r', `${25}`,
+    '-g', `${25 * 2}`,
+    '-keyint_min', 25,
+    '-crf', '25',
+    '-pix_fmt', 'yuv420p',
+    '-sc_threshold', '0',
+    '-profile:v', 'main',
+    '-level', '3.1',
+    '-c:a', 'aac',
+    '-b:a', '128k',
+    '-ar', 128000 / 4,
+    '-f', 'flv',
+    `rtmp://a.rtmp.youtube.com/live2/mjvg-hkxu-40a4-zskz-9fz0`,
 ];
 
 const ffmpegProcess = spawn('ffmpeg', options);
+
 ffmpegProcess.stdout.on('data', (data) => {
-  console.log(`ffmpeg stdout: ${data}`);
+    console.log(`ffmpeg stdout: ${data}`);
 });
 
-ffmpegProcess.stderr.on('data', (data) => {
-  console.error(`ffmpeg stderr: ${data}`);
-});
+ ffmpegProcess.stderr.on('data', (data) => {
+     console.error(`ffmpeg stderr: ${data}`);
+ });
 
-ffmpegProcess.on('close', (code) => {
-  console.log(`ffmpeg process exited with code ${code}`);
-});
+ ffmpegProcess.on('close', (code) => {
+     console.log(`ffmpeg process exited with code ${code}`);
+ });
 
 
 
@@ -47,13 +49,13 @@ app.use(express.static(path.resolve('./public')))
 
 
 io.on('connection', socket => {
-  console.log('sockrt connected' , socket.id);
-  socket.on('binarystream', stream => {
-    console.log('binary stream incoming...')
-    ffmpegProcess.stdin.write(stream, (err) => {
-      console.log('Err', err)
-     })
-  })
+    console.log('Socket Connected', socket.id);
+    socket.on('binarystream', stream => {
+        console.log('Binary Stream Incommming...')
+         ffmpegProcess.stdin.write(stream, (err) => {
+            console.log('Erhhhr', err)
+        })
+    })
 })
 
-server.listen(3000 ,()=> console.log('listening on port 3000')  );
+server.listen(3000, () => console.log(`HTTP Server is runnning on PORT 3000`))
